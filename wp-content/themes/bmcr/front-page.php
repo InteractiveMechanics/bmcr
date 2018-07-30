f616c7f2b3ff2ebb1bc6ffcc362de5244559ca42
Christina Deemer <cahdeemer@gmail.com>
2018-07-30 15:05:38 -0400

    added sass files and static home page

---
+++ wp-content/themes/bmcr/front-page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for the homepage
+ *
+ * 
+ *
+ * @package bmcr
+ */
+
+get_header();
+?>
+
+
+<main id="main" class="site-main">
+	<div class="jumbotron">
+		
+	</div>
+	
+
+</main>
+
+
+<?php get_footer(); ?>
