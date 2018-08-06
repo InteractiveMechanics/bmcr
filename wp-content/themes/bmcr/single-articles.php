070465c90e49e6c6373a55e30093e1a57700cbb5
Christina Deemer <cahdeemer@gmail.com>
2018-08-06 18:42:45 -0400

    added references to single article

--- wp-content/themes/bmcr/single-articles.php
+++ wp-content/themes/bmcr/single-articles.php
@@ -58,12 +58,26 @@
 				if( $posts ): ?>
 				    <ul>
 				    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-				        <?php setup_postdata($post); ?>
-				        <li>
-				        	<?php //TODO Configure custom fields for related publications ?>
-				            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				        </li>
-				    <?php endforeach; ?>
+				        <?php setup_postdata($post);
+				        
+				        	 
+						$post_type = get_post_type( $post->ID ); if ($post_type == 'reviews'):
+						 
+						get_template_part( 'template-parts/content', 'referencereview' );
+						
+						elseif ($post_type == 'articles'):
+						
+						get_template_part( 'template-parts/content', 'referencearticle' );
+						
+						else:
+						
+						get_template_part( 'template-parts/content', 'referenceresponse' );
+						
+						endif;
+
+
+				      
+					endforeach; ?>
 				    </ul>
 				    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
 				    
