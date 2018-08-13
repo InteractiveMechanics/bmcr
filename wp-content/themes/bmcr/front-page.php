8b9c354911668ce63cef93773424701ac09b6e3a
Christina Deemer <cahdeemer@gmail.com>
2018-08-13 15:38:18 -0400

    updated hero templates and added misc templates

--- wp-content/themes/bmcr/front-page.php
+++ wp-content/themes/bmcr/front-page.php
@@ -8,43 +8,13 @@
  */
 
 get_header();
-	$bkgr_img = get_field('bkgr_img');
-	$bkgr_img_url = $bkgr_img['url'];
-	$bkgr_img_alt = $bkgr_img['alt'];
-	$hero_title = get_field('hero_title');
 ?>
 
 
 <main id="main" class="site-main">
-	<!-- check if there is a background image -->
-	<?php if($bkgr_img): ?>
 	
-		<div class="jumbotron" style="background-image: url('<?php echo $bkgr_img_url; ?>')" alt="<?php echo $bkgr_img_alt; ?>">
-	
-	<?php else: ?>
-		
-		<div class="jumbotron">
-	
-	<?php endif; ?>
-	
-	
-	<h1><?php echo $hero_title; ?></h1>
-		
-	
-	<?php if( have_rows('hero_btns') ): 
-		
-		while ( have_rows('hero_btns') ) : the_row();
-			$hero_btn_url = get_sub_field('btn_link'); 
-			$hero_btn_text = get_sub_field('button_text');
-	?>
-        
-        	<a href="<?php echo $hero_btn_url; ?>"><?php echo $hero_btn_text; ?></a>
-	        	
-		<?php endwhile; ?>
-		
-	<? endif; ?>
-		
-	</div><!-- /.jumbotron -->
+	<?php get_template_part( 'template-parts/content', 'hero' ); ?>
+			
 	
 	<div class="recent-posts">
 		<h2>Recent Publications</h2>
---
+++ wp-content/themes/bmcr/page-hero.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Page with Hero
+ *
+ * This is the template that displays static pages that have a hero on top*
+ 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bmcr
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			
+			
+			get_template_part( 'template-parts/content', 'hero' );
+			 
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
