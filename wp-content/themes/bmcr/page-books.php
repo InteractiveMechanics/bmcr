25def45e866a2255cdacf599bab767601efdb66e
Christina Deemer <cahdeemer@gmail.com>
2018-09-27 17:24:42 -0400

    refactored books available section

--- wp-content/themes/bmcr/page-books.php
+++ wp-content/themes/bmcr/page-books.php
@@ -15,16 +15,21 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+	<div class="container-fluid">	
+		<div class="page-header" class="row">
+			<div class="col-sm-10 offset-sm-1">
+				<h2 class="page-title">Available Books</h2>
+					
+				<ul>
+					<li><a href="">All</a></li>
+					<li><a href="">Author</a></li>
+					<li><a href="">Subject</a></li>
+				</ul>
+			</div>
+		</div>
 		
-	<div class="page-header">
-		<h1 class="page-title">Available Books</h1>
-			
-		<ul>
-			<li><a href="">All</a></li>
-			<li><a href="">Author</a></li>
-			<li><a href="">Subject</a></li>
-		</ul>
-	</div>
+	
+		
 	
 	
 	<?php 
@@ -42,55 +47,16 @@ get_header();
 
 	foreach ( $myposts as $post ) : setup_postdata( $post );
 	
-	$pub_id = get_the_ID();
-	$bmcr_id = get_field('bmcr_id');
-	$pub_location = get_field('pub_location');
-	$publisher = get_field('publisher');
-	$pub_date = get_field('pub_date');
-	$isbn = get_field('isbn');
-	$book_author = get_field('book_author');
-	
-	
-	 
+	get_template_part( 'template-parts/content', 'referencereview' );
 	?>
-	<div>
-	<small>BMCR <?php echo $bmcr_id; ?></small> 
-	<h4><?php echo the_title(); ?></h4>
-	<p><?php echo $book_author; ?> 
-	<p>
-		<?php 
-			
-			if ($pub_location):
-				echo $pub_location . ': ';
-			endif;
-			
-			if ($publisher): 
-				echo $publisher . ', ';
-			endif;
-			
-			if ($pub_date):
-				echo $pub_date;
-			endif;
-			
-			if ($pub_date || $publisher): 
-				echo '|';
-			endif;
-			
-			if ($isbn):
-				echo 'ISBN ' . $isbn;
-			endif;
-		?>
 	
-	</p>
-	
-	<a href="<?php echo get_page_link(2); ?>" target="_blank">Apply to review this book</a>	
 	</div>	
 		 
 	<?php endforeach; 
 	wp_reset_postdata();
 	?>
 
-	
+	</div><!--/.container-fluid -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
