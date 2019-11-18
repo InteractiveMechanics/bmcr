3ae03168dae35a8ea3857311755ec8fe14d0813f
Matt Love <mattlovedesign@gmail.com>
2019-11-18 17:39:13 -0600

    add reviewer last name to search

--- wp-content/plugins/bmcr_admin_search/bmcr-admin-search.php
+++ wp-content/plugins/bmcr_admin_search/bmcr-admin-search.php
@@ -31,6 +31,38 @@ function custom_search_query( $query ) {
         $query->set("meta_query", $meta_query);
     };
 }
-add_filter( "pre_get_posts", "custom_search_query");
+
+
+//add wildcard string replace to subfield queries
+function my_posts_where( $where ) {
+
+	$where = str_replace("meta_key = 'reviewers_$", "meta_key LIKE 'reviewers_%", $where);
+  $where = str_replace("meta_key = 'books_$", "meta_key LIKE 'books_%", $where);
+
+	return $where;
+}
+
+add_filter('posts_where', 'my_posts_where');
+
+
+//make similar to functionality above, pass in array of search params
+function custom_search_query_2( $query ) {
+  $searchterm = $query->query_vars['s'];
+  // we have to remove the "s" parameter from the query, because it will prevent the posts from being found
+  $query->query_vars['s'] = "";
+  if ($searchterm != "") {
+    $meta_query = array(
+      array(
+			'key'		=> 'reviewers_$_reviewer_last_name',
+			'compare'	=> '=',
+			'value'		=> $searchterm,
+		)
+	);
+$query->set('meta_query', $meta_query);
+}
+}
+
+add_filter( "pre_get_posts", "custom_search_query_2");
+//add_filter( "pre_get_posts", "custom_search_query");
 
 ?>
