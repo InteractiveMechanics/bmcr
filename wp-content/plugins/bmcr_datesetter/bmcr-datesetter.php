3579de889ddb18f565aef7993f6f375529926140
Matt Love <mattlovedesign@gmail.com>
2019-11-18 18:23:35 -0600

    testing

--- wp-content/plugins/bmcr_datesetter/bmcr-datesetter.php
+++ wp-content/plugins/bmcr_datesetter/bmcr-datesetter.php
@@ -54,8 +54,8 @@ function save_datesetter($post_id, $post, $update) {
 
 
     //render date_time for email set to noon)
-    $first_reminder_date_time = date('F j, Y H:i:s', strtotime('19:25', strtotime('11-18-2019')));
-    schedule_reminder( 'mattlovedesign@gmail.com', 'test reminder 10 minutes after save', 'test reminder 1 body', '', 1, $first_reminder_date_time);
+    $first_reminder_date_time = date('F j, Y H:i:s', strtotime('19:35', strtotime('11-18-2019')));
+    schedule_reminder( 'mattlovedesign@gmail.com', 'test reminder 10 minutes after save', $first_reminder_date_time, '', 1, $first_reminder_date_time);
 
 
     // $second_reminder_date_time = date('F j, Y H:i:s', strtotime('noon', strtotime($second_reminder_date)));
